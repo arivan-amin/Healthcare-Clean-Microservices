22362c5f09a544d957bcf1a7d5783beae7ab9f6d
Arivan-Amin <arivanamin@gmail.com>
2024-08-08 15:14:24 +0300

    Add Backend Core Module

---
+++ Backend-Core/src/main/java/com/arivanamin/healthcare/Backend/Core/BackendCoreApplication.java
@@ -0,0 +1,13 @@
+package com.arivanamin.healthcare.Backend.Core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendCoreApplication.class, args);
+	}
+
+}
