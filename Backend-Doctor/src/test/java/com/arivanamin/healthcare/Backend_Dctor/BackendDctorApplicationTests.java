a743a6ce8295e4674665419bac2beba7d75d84ab
Arivan-Amin <arivanamin@gmail.com>
2024-08-12 14:35:08 +0300

    Cleanup modules

---
+++ Backend-Doctor/src/test/java/com/arivanamin/healthcare/Backend_Dctor/BackendDctorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arivanamin.healthcare.Backend_Dctor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendDctorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
