b010682fad86e1d3f9942e55536603eeeba46370
Arivan-Amin <arivanamin@gmail.com>
2024-08-31 20:22:58 +0300

    Enable observability on api gateway and Eureka

--- Api-Gateway/src/main/java/com/arivanamin/healthcare/backend/api/gateway/infrastructure/ApiGateway.java
+++ Api-Gateway/src/main/java/com/arivanamin/healthcare/backend/api/gateway/infrastructure/ApiGateway.java
@@ -1,4 +1,4 @@
-package com.arivanamin.healthcare.backend.api.gateway;
+package com.arivanamin.healthcare.backend.api.gateway.infrastructure;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
