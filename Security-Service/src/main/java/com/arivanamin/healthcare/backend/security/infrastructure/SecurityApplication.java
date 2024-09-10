74eb5abcaee93cd9f0560429128a049dcc39a009
Arivan-Amin <arivanamin@gmail.com>
2024-09-10 11:26:48 +0300

    Modify main class to scan core packages

--- Security-Service/src/main/java/com/arivanamin/healthcare/backend/security/infrastructure/SecurityApplication.java
+++ Security-Service/src/main/java/com/arivanamin/healthcare/backend/security/infrastructure/SecurityApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@SpringBootApplication (scanBasePackages = "com.arivanamin.healthcare.backend")
 @EnableDiscoveryClient
 @EnableCaching
 public class SecurityApplication {
