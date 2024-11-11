a7ab21d012dff501c0da39da101a988bfb5b5b04
Arivan-Amin <arivanamin@gmail.com>
2024-11-11 15:46:42 +0300

    Add patient persistence tests

--- Patient-Service/src/main/java/com/arivanamin/healthcare/backend/patient/details/JpaPatient.java
+++ Patient-Service/src/main/java/com/arivanamin/healthcare/backend/patient/details/JpaPatient.java
@@ -11,6 +11,7 @@ import java.time.LocalDate;
 import java.util.UUID;
 
 @Entity
+@Table (name = "patients")
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
@@ -22,7 +23,7 @@ class JpaPatient {
     private static ModelMapper mapper = new ModelMapper();
     
     @Id
-    @GeneratedValue (strategy = GenerationType.AUTO)
+    @GeneratedValue
     UUID id;
     
     @NotBlank
