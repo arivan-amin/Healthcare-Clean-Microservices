f2f3ba31630d14326102c188fba307aea78d589d
Arivan-Amin <arivanamin@gmail.com>
2024-11-26 20:48:36 +0300

    Add JpaPatientPersistence tests

--- Patient-Service/src/test/java/com/arivanamin/healthcare/backend/patient/details/JpaPatientPersistenceIntegrationTest.java
+++ Patient-Service/src/test/java/com/arivanamin/healthcare/backend/patient/details/JpaPatientPersistenceIntegrationTest.java
@@ -2,7 +2,6 @@ package com.arivanamin.healthcare.backend.patient.details;
 
 import com.arivanamin.healthcare.backend.patient.core.entity.Patient;
 import com.arivanamin.healthcare.backend.testing.architecture.bases.BaseIntegrationTest;
-import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -12,9 +11,7 @@ import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@Tag ("current")
-@DataJpaTest
-@Slf4j
+@DataJpaTest (showSql = false)
 class JpaPatientPersistenceIntegrationTest implements BaseIntegrationTest {
     
     @Autowired
@@ -23,21 +20,25 @@ class JpaPatientPersistenceIntegrationTest implements BaseIntegrationTest {
     private JpaPatientPersistence persistence;
     private int numberOfSavedEntities;
     
-    private UUID actualId;
-    private JpaPatient actualPatient;
-    private List<Patient> resultPatients;
-    private Patient resultPatient;
+    private UUID expectedId;
+    private List<Patient> expectedPatients;
+    private Patient expectedPatient;
     
     @BeforeEach
     void setUp () {
         persistence = new JpaPatientPersistence(repository);
     }
     
+    @AfterEach
+    void tearDown () {
+        repository.deleteAll();
+    }
+    
     @Test
     void shouldReturnAllPatientsWhenFindAllIsCalled () {
         givenRepositoryWithSavedPatients();
         whenFindAllIsCalled();
-        thenAssertThatAllEntitiesOfRepositoryAreReturned(resultPatients);
+        thenAssertThatAllEntitiesOfRepositoryAreReturned(expectedPatients);
     }
     
     private void givenRepositoryWithSavedPatients () {
@@ -48,7 +49,7 @@ class JpaPatientPersistenceIntegrationTest implements BaseIntegrationTest {
     }
     
     private void whenFindAllIsCalled () {
-        resultPatients = persistence.findAll();
+        expectedPatients = persistence.findAll();
     }
     
     private void thenAssertThatAllEntitiesOfRepositoryAreReturned (List<Patient> result) {
@@ -58,36 +59,78 @@ class JpaPatientPersistenceIntegrationTest implements BaseIntegrationTest {
     @Test
     void shouldReturnSinglePatientWhenFindByIdIsCalled () {
         givenRepositoryWithSamplePatientsAndOnePatientExtracted();
-        whenFindByIdIsCalled(actualId);
-        thenAssertThatCorrectPatientIsReturned(resultPatient);
+        whenFindByIdIsCalled(expectedId);
+        thenAssertThatCorrectPatientIsReturned(expectedPatient);
     }
     
     private void givenRepositoryWithSamplePatientsAndOnePatientExtracted () {
         givenRepositoryWithSavedPatients();
-        actualId = repository.findAll()
+        expectedId = repository.findAll()
             .get(FAKER.number().numberBetween(0, numberOfSavedEntities))
             .getId();
-        actualPatient = repository.findAll()
-            .stream()
-            .filter(jpaPatient -> jpaPatient.getId().equals(actualId))
-            .findFirst()
-            .orElseThrow();
+        expectedPatient = repository.findAll()
+            .stream().filter(patient -> patient.getId().equals(expectedId))
+            .findFirst().orElseThrow().toDomain();
     }
     
     private void whenFindByIdIsCalled (UUID sampleId) {
-        resultPatient = persistence.findById(sampleId).orElseThrow();
+        expectedPatient = persistence.findById(sampleId).orElseThrow();
     }
     
     private void thenAssertThatCorrectPatientIsReturned (Patient resultPatient) {
-        assertThat(resultPatient).isEqualTo(actualPatient.toDomain());
+        assertThat(resultPatient).isEqualTo(expectedPatient);
     }
     
     @Test
-    void shouldCreatePatientWhenCreateIsCalled () {
-        // given
-        
-        // when
-        
-        // then
+    void shouldDeletePatientInRepositoryWhenDeleteIsCalled () {
+        givenRepositoryWithSamplePatientsAndOnePatientExtracted();
+        whenDeleteIsCalled();
+        thenAssertThatEntityIsDeletedFromRepository();
+    }
+    
+    private void whenDeleteIsCalled () {
+        persistence.delete(expectedId);
+    }
+    
+    private void thenAssertThatEntityIsDeletedFromRepository () {
+        assertThat(persistence.findAll().size()).isEqualTo(numberOfSavedEntities - 1);
+        assertThat(repository.findById(expectedId)).isEmpty();
+    }
+    
+    @Test
+    void shouldSavePatientInRepositoryWhenCreateIsCalled () {
+        givenValidSamplePatient();
+        whenCreateIsCalled();
+        thenAssertThatPatientIsCreatedInRepository();
+    }
+    
+    private void givenValidSamplePatient () {
+        expectedPatient = RANDOM.nextObject(Patient.class);
+        expectedPatient.setEmail(FAKER.internet().emailAddress());
+    }
+    
+    private void whenCreateIsCalled () {
+        persistence.create(expectedPatient);
+    }
+    
+    private void thenAssertThatPatientIsCreatedInRepository () {
+        assertThat(repository.findAll().size()).isOne();
+    }
+    
+    @Test
+    void shouldUpdatePatientInRepositoryWhenUpdateIsCalled () {
+        givenRepositoryWithSamplePatientsAndOnePatientExtracted();
+        whenUpdateIsCalledWithModifiedPatient();
+        thenAssertThatPatientIsUpdatedInRepository();
+    }
+    
+    private void whenUpdateIsCalledWithModifiedPatient () {
+        expectedPatient.setFirstName(FAKER.zelda().character());
+        persistence.update(expectedPatient);
+    }
+    
+    private void thenAssertThatPatientIsUpdatedInRepository () {
+        JpaPatient result = repository.findById(expectedId).orElseThrow();
+        assertThat(result.getFirstName()).isEqualTo(expectedPatient.getFirstName());
     }
 }
--- Patient-Service/src/test/java/com/arivanamin/healthcare/backend/patient/details/JpaPatientPersistenceTest.java
+++ Patient-Service/src/test/java/com/arivanamin/healthcare/backend/patient/details/JpaPatientPersistenceTest.java
@@ -1,12 +1,25 @@
 package com.arivanamin.healthcare.backend.patient.details;
 
 import com.arivanamin.healthcare.backend.testing.architecture.bases.BaseUnitTest;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@Tag ("current")
+@ExtendWith (MockitoExtension.class)
 class JpaPatientPersistenceTest implements BaseUnitTest {
     
+    @Mock
+    PatientRepository repository;
+    
+    @InjectMocks
+    JpaPatientPersistence persistence;
+    
     @Test
-    void should () {
+    void shouldReturnAllPatientsFromRepository () {
         // given
         
         // when
