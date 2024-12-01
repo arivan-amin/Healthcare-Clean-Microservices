5bd126b892ad83f7b4b4fda3c73d4a46eaac7d8d
Arivan-Amin <arivanamin@gmail.com>
2024-12-01 17:26:54 +0300

    Add liquibase script

--- Patient-Service/src/main/java/com/arivanamin/healthcare/backend/patient/application/config/SpringBeansConfig.java
+++ Patient-Service/src/main/java/com/arivanamin/healthcare/backend/patient/application/config/SpringBeansConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
-public class SpringBeansConfig {
+class SpringBeansConfig {
     
     @Bean
     public PatientPersistence persistence (PatientRepository repository) {
