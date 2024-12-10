7d724a6063d7a66be0b59fdbf70c3de38eee6f5b
Arivan-Amin <arivanamin@gmail.com>
2024-12-10 18:58:39 +0300

    Create bean config for Liquibase

---
+++ Patient-Service/src/main/java/com/arivanamin/healthcare/backend/patient/application/config/PatientLiquibaseConfig.java
@@ -0,0 +1,19 @@
+package com.arivanamin.healthcare.backend.patient.application.config;
+
+import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import static com.arivanamin.healthcare.backend.base.domain.config.CoreApplicationConfig.LIQUIBASE_CHANGELOG_PATH;
+
+@Configuration
+class PatientLiquibaseConfig {
+    
+    @Bean
+    public SpringLiquibase liquibase () {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setChangeLog(LIQUIBASE_CHANGELOG_PATH);
+        liquibase.setShouldRun(true);
+        return liquibase;
+    }
+}
