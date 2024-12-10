86718a85bd4c907f6a7ecc633a5e7b739dacc362
Arivan-Amin <arivanamin@gmail.com>
2024-12-10 14:12:49 +0300

    Bump swagger version

--- Api-Gateway/src/main/java/com/arivanamin/healthcare/backend/api/gateway/application/ApiGateway.java
+++ Api-Gateway/src/main/java/com/arivanamin/healthcare/backend/api/gateway/application/ApiGateway.java
@@ -2,12 +2,10 @@ package com.arivanamin.healthcare.backend.api.gateway.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableCaching
 public class ApiGateway {
     
     public static void main (String[] args) {
