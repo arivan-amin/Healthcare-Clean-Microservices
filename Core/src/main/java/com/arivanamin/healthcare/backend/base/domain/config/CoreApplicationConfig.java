7fd84d9ced674c1578f6926f8cde606146b1edde
Arivan-Amin <arivanamin@gmail.com>
2024-12-10 18:58:17 +0300

    Remove unnecessary classes from Core

--- Core/src/main/java/com/arivanamin/healthcare/backend/base/domain/config/CoreApplicationConfig.java
+++ Core/src/main/java/com/arivanamin/healthcare/backend/base/domain/config/CoreApplicationConfig.java
@@ -1,24 +1,27 @@
 package com.arivanamin.healthcare.backend.base.domain.config;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
 import java.nio.file.Path;
 import java.time.ZoneId;
 
 import static java.lang.System.getProperty;
 import static java.nio.file.Paths.get;
 
+@NoArgsConstructor (access = AccessLevel.PRIVATE)
 public final class CoreApplicationConfig {
     
     public static final String APPLICATION_DIRECTORY_NAME = "Healthcare";
     
     public static final String BASE_PACKAGE = "com.arivanamin.healthcare.backend";
     
+    public static final String LIQUIBASE_CHANGELOG_PATH =
+        "classpath:db/changelog/changelog-master.xml";
+    
     public static final String APP_TIMEZONE = "Asia/Baghdad";
     public static final ZoneId ZONE_ID = ZoneId.of(APP_TIMEZONE);
-    
     static final String USER_HOME_DIRECTORY_PROPERTY = "user.home";
-    
-    private CoreApplicationConfig () {
-    }
     
     public static Path getApplicationConfigDirectory () {
         return get(getUserHomeDirectory(), "Apps", APPLICATION_DIRECTORY_NAME);
