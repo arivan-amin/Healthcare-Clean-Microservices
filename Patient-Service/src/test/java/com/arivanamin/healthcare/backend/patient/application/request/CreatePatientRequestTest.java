f35a547dddaf199e5a6a63789ea8e8a8209bed63
Arivan-Amin <arivanamin@gmail.com>
2024-12-25 21:32:24 +0300

    Fix patient service tests

--- Patient-Service/src/test/java/com/arivanamin/healthcare/backend/patient/application/request/CreatePatientRequestTest.java
+++ Patient-Service/src/test/java/com/arivanamin/healthcare/backend/patient/application/request/CreatePatientRequestTest.java
@@ -2,16 +2,23 @@ package com.arivanamin.healthcare.backend.patient.application.request;
 
 import com.arivanamin.healthcare.backend.patient.core.entity.Patient;
 import com.arivanamin.healthcare.backend.testing.architecture.bases.BaseUnitTest;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDateTime;
+
+import static com.arivanamin.healthcare.backend.base.domain.dates.TimestampHelper.toLocalDateTime;
+import static com.arivanamin.healthcare.backend.base.domain.dates.TimestampHelper.toTimestamp;
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Slf4j
 class CreatePatientRequestTest implements BaseUnitTest {
     
     @Test
     void shouldMapRequestToEntityCorrectly () {
         // given
         CreatePatientRequest request = RANDOM.nextObject(CreatePatientRequest.class);
+        request.setDateOfBirth(toTimestamp(LocalDateTime.now()));
         
         // when
         Patient entity = request.toEntity();
@@ -20,7 +27,8 @@ class CreatePatientRequestTest implements BaseUnitTest {
         assertThat(request.getFirstName()).isEqualTo(entity.getFirstName());
         assertThat(request.getLastName()).isEqualTo(entity.getLastName());
         assertThat(request.getEmail()).isEqualTo(entity.getEmail());
-        assertThat(request.getDateOfBirth()).isEqualTo(entity.getDateOfBirth());
+        assertThat(toLocalDateTime(request.getDateOfBirth()).toLocalDate()).isEqualTo(
+            entity.getDateOfBirth());
         assertThat(request.getGender()).isEqualTo(entity.getGender());
         assertThat(request.getAddress()).isEqualTo(entity.getAddress());
     }
