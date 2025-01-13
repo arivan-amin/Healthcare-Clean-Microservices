68ee69e9a1783f4b7d802161ebc65699a26f9a4c
Arivan-Amin <arivanamin@gmail.com>
2025-01-13 17:26:15 +0300

    Cleanup annotations

--- Patient-Service/src/test/java/com/arivanamin/healthcare/backend/patient/storage/JpaPatientStorageTest.java
+++ Patient-Service/src/test/java/com/arivanamin/healthcare/backend/patient/storage/JpaPatientStorageTest.java
@@ -1,15 +1,10 @@
 package com.arivanamin.healthcare.backend.patient.storage;
 
 import com.arivanamin.healthcare.backend.testing.architecture.bases.BaseUnitTest;
-import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 
-@Tag ("current")
-@ExtendWith (MockitoExtension.class)
 class JpaPatientStorageTest implements BaseUnitTest {
     
     @Mock
--- Testing/src/main/java/com/arivanamin/healthcare/backend/testing/architecture/bases/BaseUnitTest.java
+++ Testing/src/main/java/com/arivanamin/healthcare/backend/testing/architecture/bases/BaseUnitTest.java
@@ -2,7 +2,11 @@ package com.arivanamin.healthcare.backend.testing.architecture.bases;
 
 import com.github.javafaker.Faker;
 import org.jeasy.random.EasyRandom;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@SuppressWarnings ("NewClassNamingConvention")
+@ExtendWith (MockitoExtension.class)
 public interface BaseUnitTest {
     
     Faker FAKER = new Faker();
